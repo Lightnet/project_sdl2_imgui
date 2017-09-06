81dbd83efa28560895f4c91ead7432d6d64109fc
Lightnet <darkneter@gmail.com>
2017-09-06 10:42:56 -0700

    update the build batch.

--- vsbuild.bat
+++ vsbuild.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-cd ../../../projects/project_cgui
+cd ../../../projects/project_sdl_imgui
 call scons tool=window
 REM pause
